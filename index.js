30be7a9c5a1942c63d932fd231bb4dca857bf366
RowHunt <yearwoodk9@gmail.com>
2020-11-04 17:17:52 -0500

    GitHub Pages Test

---
+++ index.js
@@ -0,0 +1,24 @@
+
+// (function($) {
+//     let quotes = $(".quote-scroll p");
+//     alert(quotes.length)
+
+
+
+
+//   })(jQuery);
+
+
+  /*
+
+1. get a collection of P tags within quote scroll
+2. set in html and assume in javascript that the first p i n your collection has a class set to fadein
+3. create a loop that shopws an alert for the content of each P tag
+
+
+
+
+
+
+
+  */
